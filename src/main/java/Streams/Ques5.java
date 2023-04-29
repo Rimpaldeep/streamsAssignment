eb3a246265ff8639596215ae5d138fb290716d9d
Rimpaldeep <rimgill007@gmail.com>
2023-04-29 11:23:48 -0400

    modified ques5

--- src/main/java/Streams/Ques5.java
+++ src/main/java/Streams/Ques5.java
@@ -8,10 +8,11 @@ import java.util.List;
 public class Ques5 {
 
     public static void main(String[] args) {
-        User user1 = new User ("Rimpaldeep", "Kaur", "India", Collections.singleton ("Amritsar, Kerala"));
-        User user2 = new User ("John", "Kasunic", "Canada", Collections.singleton ("Toronto, Brampton, Scarborough"));
-        User user3 = new User ("Adam", "Robinson", "Canada", Collections.singleton ("Toronto, Brampton, Scarborough"));
-        List<String> list = new ArrayList<String> ();
+        List <User> list = new ArrayList<> ();
+        list.add (new User ("Rimpaldeep", "Kaur", "India", Collections.singleton ("Amritsar, Kerala")));
+        list.add (new User ("John", "Kasunic", "Canada", Collections.singleton ("Toronto, Brampton, Scarborough")));
+        list.add (new User ("Adam", "Robinson", "Canada", Collections.singleton ("Toronto, Brampton, Scarborough")));
+        System.out.println (list);
         
     }
 }
